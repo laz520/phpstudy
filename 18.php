6ab4d3c28ed6f1bd6b31f64b049fa3910d92ccd0
16750145927 <2104819695@qq.com>
2022-04-11 18:13:15 +0800

    ·

--- 18.php
+++ 18.php
@@ -10,8 +10,37 @@ $a = [['title' => '名字', 'qq' => '2104819596', 'weixing' => 'weiex']
     ,['title' => '名字', 'qq' => '2104819596', 'weixing' => 'weiex']
 
     ,];
-foreach ($a  as $c){
-    echo  $c['title']  , '<br> ';
-    echo  $c['qq'] , '<br>';
-    echo $c['weixing'];
-}
+
+?>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+
+    }
+
+   .box1{
+        width: 400px;
+        height: 200px;
+        background-color: skyblue;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 10px;
+
+    }
+</style>
+<?php
+foreach ($a as $c){
+?>
+<div class="box">
+<div class="box1">
+    <h3>一个个人简介</h3>
+    <p><?php echo $c['title']?></p>
+    <p><?php echo $c['qq']?></p>
+    <p><?php echo $c['weixing']?></p>
+</div>
+</div>
+<?php
+}?>
